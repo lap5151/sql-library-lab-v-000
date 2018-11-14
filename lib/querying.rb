f16730627ae7c5ffe996b31ef9f6a2e25d5d2942
Lauren Pardee <lap5151@gmail.com>
2018-11-14 20:26:23 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ INNER JOIN subgenres ON subgenres.id = series.subgenre_id
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT species,count(species) FROM characters WHERE series_id = 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
